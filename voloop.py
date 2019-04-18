55dc035b5c067956a8846ffed53c5555612027ab
gshorten <geoff.shorten@shortens.ca>
2019-04-18 08:35:48 -0600

    added line to actually change the volume!

--- voloop.py
+++ voloop.py
@@ -364,8 +364,8 @@ class VolumeControl:
         unit_volume = self.unit.volume
         print('Current Volume: ', unit_volume)
         print("Time: ", time.time())
-        print("encoder a, encoder b : ", encoder_a,encoder_b)
-        print("encoder a, encoder_b_old", encoder_a, self.encoder_b_old)
+        print("encoder a, encoder a old: ", encoder_a, self.encoder_a_old)
+        print("encoder b, encoder_b_old: ", encoder_b, self.encoder_b_old)
         # if (encoder_a,self.encoder_b_old) == (1, 0) or (encoder_a, self.encoder_b_old) == (0,1):
         if (encoder_a, encoder_b) == (1, 0):
             # this will be clockwise rotation
@@ -385,6 +385,7 @@ class VolumeControl:
         # unit.volume = unit_volume
 
         self.encoder_b_old = encoder_b
+        self.encoder_a_old = encoder_a
         # print('New Volume: ', unit_volume)
 
 def playstate(unit):
