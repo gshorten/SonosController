779b39147633602a8df069d6155e7f6cd81c8c38
gshorten <geoff.shorten@shortens.ca>
2019-04-18 21:31:14 -0600

    added line to actually change the volume!

--- voloop.py
+++ voloop.py
@@ -357,8 +357,8 @@ class VolumeControl:
         self.count = 0
 
         # set up the callback function
-        GPIO.add_event_detect(self.enc_a, GPIO.FALLING, callback=self.volume_set, bouncetime=50)  # Encoder A
-        GPIO.add_event_detect(self.enc_b, GPIO.FALLING, callback=self.volume_set, bouncetime=50)  # Encoder B
+        GPIO.add_event_detect(self.enc_a, GPIO.FALLING, callback=self.volume_set, bouncetime=25)  # Encoder A
+        GPIO.add_event_detect(self.enc_b, GPIO.FALLING, callback=self.volume_set, bouncetime=25)  # Encoder B
 
     def volume_set(self,channel):
         encoder_a, encoder_b = GPIO.input(self.enc_a), GPIO.input(self.enc_b)
