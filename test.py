48c120a613b11975dc8972a0f83111f2eb970411
gshorten <geoff.shorten@shortens.ca>
2019-04-22 16:26:29 -0600

    debugging vol control - trying to find better algorithm

--- test.py
+++ test.py
@@ -3,37 +3,46 @@ import RGBRotaryEncoder
 import RPi.GPIO as GPIO
 import soco
 
-def change_volume(event):
-    # callback function to change the volume of the sonos unit
-    # is called from the VolControl object
+
+class VolumeControl:
     new_volume = 0
-    # get the volume of the sonos unit
-    unit_volume = unit.volume
-    # increment the volume up or down based on event value
-    # can be 1 or 2
-    # also limit volume to between 0 and 100
-    if event == 1:
-        # direction is clockwise
-        new_volume = unit_volume + 4
-        if new_volume > 100:
-            new_volume = 100
-        unit.volume = new_volume
-    elif event == 2:
-        # direction is counter clockwise, volume down
-        # turn volume down more quickly than up, better for the user!
-        new_volume = unit_volume - 5
-        if new_volume < 0:
-            new_volume = 0
-        unit.volume = new_volume
-    print ("new volume: ", new_volume)
+
+    def __init__(self,unit,event):
+        self.unit = unit
+
+    def change_volume(self,event):
+        # callback function to change the volume of the sonos unit
+        # is called from the VolControl object
+        new_volume = 0
+        # get the volume of the sonos unit
+        unit_volume = self.unit.volume
+        # increment the volume up or down based on event value
+        # can be 1 or 2
+        # also limit volume to between 0 and 100
+        if event == 1:
+            # direction is clockwise
+            new_volume = unit_volume + 4
+            if new_volume > 100:
+                new_volume = 100
+            self.unit.volume = new_volume
+        elif event == 2:
+            # direction is counter clockwise, volume down
+            # turn volume down more quickly than up, better for the user!
+            new_volume = unit_volume - 5
+            if new_volume < 0:
+                new_volume = 0
+            self.unit.volume = new_volume
+        print ("new volume: ", new_volume)
 
 
 # assign sonos player to unit object
 unit = soco.SoCo('192.168.0.21')        # portable
 
+# create sonos volume control object
+SonosVolControl = VolumeControl(unit)
 
-# create volume control object
-VolControl = RGBRotaryEncoder.RotaryEncoder(19,26,4,change_volume)
+# create rotary encoder object
+VolControl = RGBRotaryEncoder.RotaryEncoder(19, 26, 4, VolumeControl.change_volume())
 
 while True:
     try:
