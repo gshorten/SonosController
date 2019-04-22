0188b225021179e5479989a1df135a719e6c9e09
gshorten <geoff.shorten@shortens.ca>
2019-04-22 08:57:36 -0600

    debugging vol control - trying to find better algorithm

--- testvolsm.py
+++ testvolsm.py
@@ -220,7 +220,7 @@ class RotaryEncoder:
 def changevolume(event):
     new_volume = 0
     unit_volume = unit.volume
-    time.sleep(.05)
+    
     if event == 1:
         # direction is clockwise
         new_volume = unit_volume + 3
