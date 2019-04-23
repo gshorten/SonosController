19a4df63afbba321e1fcb8a82b411ad843144133
gshorten <geoff.shorten@shortens.ca>
2019-04-23 06:49:03 -0600

    debugging vol control - trying to find better algorithm

--- classtest.py
+++ classtest.py
@@ -2,6 +2,7 @@
 import RGBRotaryEncoder as Encoder
 import RPi.GPIO as GPIO
 import soco
+import time
 
 class SonosVolCtrl:
     # processes the callback from the rotary encoder to change the volume of the sonos unit
@@ -11,6 +12,11 @@ class SonosVolCtrl:
         self.unit = sonos_unit
         self.upinc = up_increment   # how much to change the volume each click of the volume knob
         self.downinc = down_increment   #how much to change the volume down
+        self.button_down = 0
+        self.button_timer = 0
+        self.button_up = 0
+        self.button_type = ""
+
 
     def change_volume(self,event):
         # callback function to change the volume of the sonos unit
@@ -36,18 +42,43 @@ class SonosVolCtrl:
                 new_volume = 0
             self.unit.volume = new_volume
             print ("new volume: ", new_volume)
-        elif event == 3:
-            print("button down")
-        elif event == 4:
-            print("button up")
+        elif event == 3 or event ==4:
+            if self.button_press(self.button_type) == 'short':
+                # short button press, pause or play sonos unit
+                self.pause_play(self)
+            elif self.button_press(self.button_type) == "long":
+                # long button press, skip to the next track
 
+    def button_press(self, event):
+        #determine if the button is pressed for a long or short press
+        #return "short" or "long"
+        if self.event == 3:
+            self.button_down = time.time()
+        if self.event == 4:
+            self.button_up = time.time()
+        self.button_timer = self.button_up - self.button_down
+        if self.button_timer < .25:
+            self.button_type = "short"
+        elif self.button_timer >= .25:
+            self.button_type = "long"
+        print(self.button_type, "button press")
+        return self.button_type
 
+    def pause_play(self):
+     unit_state = self.unit.get_current_transport_info()
+     play_state = unit_state['current_transport_state']
+     if play_state == "PAUSED_PLAYBACK" or play_state == "STOPPED":
+         self.unit.play()
+         print("Playing")
+     elif play_state == "PLAYING":
+         self.unit.pause()
+         print("Paused")
 
 
 # assign sonos player to unit object
 unit = soco.SoCo('192.168.0.21')        # portable
 # create sonos volume control knob instance
-VolumeKnob = SonosVolCtrl(unit,4,5)
+VolumeKnob = SonosVolCtrl(unit, 4, 5)
 # create rotary encoder instance
 RotaryVol = Encoder.RotaryEncoder(19, 26, 4, VolumeKnob.change_volume)
 
