7499209a41d40aa07b3e28be2ebbc70a9f4ac874
gshorten <geoff.shorten@shortens.ca>
2019-04-24 07:40:23 -0600

    debugging vol control - trying to find better algorithm

--- events.py
+++ events.py
@@ -4,7 +4,7 @@ import soco
 from soco.events import event_listener
 print ("Events listener test")
 device = soco.SoCo('192.168.1.21')
-print (device.player_name)
+print (soco.SoCo.player_name)
 sub = device.renderingControl.subscribe()
 sub2 = device.avTransport.subscribe()
 
