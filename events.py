2d66919d2581186e321f8ec696c69106157bb34b
gshorten <geoff.shorten@shortens.ca>
2019-04-24 07:23:17 -0600

    debugging vol control - trying to find better algorithm

--- events.py
+++ events.py
@@ -1,27 +1,31 @@
+from __future__ import print_function
+try:
+    from queue import Empty
+except:  # Py2.7
+    from Queue import Empty
 
 import soco
 from pprint import pprint
 from soco.events import event_listener
 # pick a device at random
-device = soco.discovery.by_name("Portable")
+device = soco.discover.by_name('Portable')
 print (device.player_name)
 sub = device.renderingControl.subscribe()
 sub2 = device.avTransport.subscribe()
 
 while True:
     try:
-        try:
-            event = sub.events.get(timeout=0.5)
-            pprint ("RenderingControl Events: ", event.variables)
-            print("******************************************")
-        except:
-            pass
-        try:
-            event = sub2.events.get(timeout=0.5)
-            pprint ("avTransport Events: ", event.variables)
-            print("******************************************")
-        except :
-            pass
+        event = sub.events.get(timeout=0.5)
+        pprint (event.variables)
+        pprint()
+    except Empty:
+        pass
+    try:
+        event = sub2.events.get(timeout=0.5)
+        pprint (event.variables)
+        pprint()
+    except Empty:
+        pass
 
     except KeyboardInterrupt:
         sub.unsubscribe()
