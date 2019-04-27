b1d1d1939a8dcf4eea5dd51b9680403219cdef43
gshorten <geoff.shorten@shortens.ca>
2019-04-26 19:02:18 -0600

    moving all code into sub modules and classes

--- SonosHW.py
+++ SonosHW.py
@@ -279,10 +279,10 @@ class KnobLED:
 
 class ExtendedLCD(LCD):
     # adds functions to the standard adafruit lcd, such as trucating and centering text.
-    lcd = LCD.Adafruit_CharLCDPlate()
+
 
     def __init__(self):
-        pass
+        self.lcd = LCD.Adafruit_CharLCDPlate()
 
     def display_stuff(self, line1, line2, duration=5):
         # displays two lines of text, sets display time out timer, turns on backlight
