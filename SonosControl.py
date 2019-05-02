244c817717bc60cece51557627caa522a60f7c78
gshorten <geoff.shorten@shortens.ca>
2019-05-02 09:25:10 -0600

    moving all code into sub modules and classes

--- SonosControl.py
+++ SonosControl.py
@@ -241,6 +241,7 @@ class SonosUnits():
         self.unit_index = 0                 # counter for stepping through list
         #self.default = default              # default sonos unit name
         self.active_unit = soco.discovery.by_name(default)    # get default sonos unit
+        time.sleep(2)
         self.lcd = lcd                      # the lcd display
         self.selected_unit_name = ''             # currently selected (but not yet active) unit attribute
         self.get_units_time = 0             # time that the sonos list was last updated
