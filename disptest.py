0ed3bf591aa5bdd5584ae4e497a528bfd65c346f
gshorten <geoff.shorten@shortens.ca>
2019-05-04 07:32:46 -0600

    Stable version.  Cleaning up documentation.

--- disptest.py
+++ disptest.py
@@ -9,8 +9,8 @@ import time
 lcd_columns = 16
 lcd_rows = 2
 i2c = busio.I2C(board.SCL, board.SDA)
-#lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
-lcd = character_lcd.Character_LCD_I2C(i2c, lcd_columns,lcd_rows)
+lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
+#lcd = character_lcd.Character_LCD_I2C(i2c, lcd_columns,lcd_rows)
 #lcd = character_lcd.Character_LCD_Mono_I2C(i2c, lcd_columns, lcd_rows)
 
 #lcd.color = [100, 0, 0]
