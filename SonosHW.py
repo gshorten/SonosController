9cfd7048a6cdd89cf1393974c2532dc128e4d61c
gshorten <geoff.shorten@shortens.ca>
2019-05-05 16:13:30 -0600

    new wallbox code - oop based. first try

--- SonosHW.py
+++ SonosHW.py
@@ -435,8 +435,8 @@ class ExtendedAdafruitI2LCD(i2c_lcd,LCD):
         self.LCD_COLUMNS = 16
         self.LCD_ROWS = 2
         LCD.__init__(self)
-        self.lcd=i2c_lcd.Character_LCD_RGB_I2C(self.i2c, 16, 2)
-        #i2c_lcd.Character_LCD_RGB_I2C.__init__(busio(board.SCL, board.SDA), 16,2)
+        #i2c_lcd.Character_LCD_RGB_I2C(self.i2c, 16, 2)
+        i2c_lcd.Character_LCD_RGB_I2C.__init__(self.busio(board.SCL, board.SDA), 16, 2)
         self.timeout = timeout  # default backlight timeout
         self.display_start_time = time.time()
 
