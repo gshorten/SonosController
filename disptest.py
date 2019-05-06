ce55c0bc20b723cba5a420f649540dab53a76e68
gshorten <geoff.shorten@shortens.ca>
2019-05-06 11:04:05 -0600

    new wallbox code - oop based. first try

--- disptest.py
+++ disptest.py
@@ -3,26 +3,22 @@ import busio
 
 from adafruit_character_lcd.character_lcd_rgb_i2c import Character_LCD_RGB_I2C
 import time
-#import adafruit_character_lcd.Character_LCD_RGB_I2C as character_lcd
-#import adafruit_character_lcd.character_lcd_i2c as character_lcd
+
 
 
 lcd_columns = 16
 lcd_rows = 2
 i2c = busio.I2C(board.SCL, board.SDA)
 lcd = Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
-#lcd = character_lcd.Character_LCD_I2C(i2c, lcd_columns,lcd_rows)
-#lcd = character_lcd.Character_LCD_Mono_I2C(i2c, lcd_columns, lcd_rows)
 
-#lcd.color = [100, 0, 0]
-#lcd.backlight = True
+
 while True:
     try:
         lcd.color = [100,0,0]
         lcd.message = "Hello\nCircuitPython"
         #lcd.move_right()
         #lcd.blink = True
-        time.sleep(.5)
+        time.sleep(2)
         lcd.clear()
         #lcd.color = [0,0,0]
 
