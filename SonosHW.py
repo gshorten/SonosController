f33ce9954c9d247862a0d31ebc9dc1c3bf2bff52
gshorten <geoff.shorten@shortens.ca>
2019-05-07 15:51:09 -0600

    new wallbox code - oop based. first try

--- SonosHW.py
+++ SonosHW.py
@@ -226,6 +226,7 @@ class RotaryEncoder:
         """
         # Grab state of input pins.
         self.pin_state = (GPIO.input(self.pinB) << 1) | GPIO.input(self.pinA)
+        print(self.pin_state)
         # Determine new state from the pins and state table.
         self.state = self.STATE_TAB[self.state & 0xf][self.pin_state]
         # Return emit bits, ie the generated event.
