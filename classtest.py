51c595f494bfd0db2d76c701efcbd7672fc65ee8
gshorten <geoff.shorten@shortens.ca>
2019-05-06 19:34:40 -0600

    new wallbox code - oop based. first try

---
+++ classtest.py
@@ -0,0 +1,44 @@
+
+"""
+testing creating subclasses of adafruit i2c lcd modules
+"""
+
+# the code below works
+import board
+import busio
+
+import adafruit_character_lcd.character_lcd_rgb_i2c
+import time
+
+lcd_columns = 16
+lcd_rows = 2
+i2c = busio.I2C(board.SCL, board.SDA)
+lcd =adafruit_character_lcd.character_lcd_rgb_i2c.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
+
+while True:
+    try:
+        lcd.color = [100, 0, 0]
+        lcd.message = "Hello\nCircuitPython"
+        time.sleep(5)
+        lcd.clear()
+
+    except KeyboardInterrupt:
+        lcd.color = [0,0,0]
+        lcd.clear()
+
+
+# now try it with a class
+
+# class TestLCD(Character_LCD_RGB_I2C):
+#
+#     def __init__(self):
+#         lcd_columns = 16
+#         lcd_rows = 2
+#         i2c = busio.I2C(board.SCL, board.SDA)
+#         adafruit_character_lcd.character_lcd_rgb_i2c.Character_LCD_RGB.__init__()
+#         adafruit_character_lcd.character_lcd_rgb_i2c.Character_LCD_RGB_I2C.__init__(self,i2c,lcd_columns,lcd_rows)
+#
+#
+#     def display_something(self, text):
+#         self.color = [100,0,0]
+#         self.
