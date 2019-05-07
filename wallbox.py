b8954950195b7340581d0ea799e111b653a29787
gshorten <geoff.shorten@shortens.ca>
2019-05-07 15:47:47 -0600

    new wallbox code - oop based. first try

--- wallbox.py
+++ wallbox.py
@@ -37,7 +37,7 @@ WallboxPlaystateLED = SonosControl.PlaystateLED(Units, 27, 17, 18)
 WallboxRotaryControl = SonosControl.SonosVolCtrl(units=Units, lcd=WallboxLCD,
                                                  vol_ctrl_led=WallboxPlaystateLED, up_increment=4, down_increment=5)
 # Rotary Encoder
-VolumeKnob = SonosHW.RotaryEncoder(pinA=21, pinB=19, rotary_callback=WallboxRotaryControl.change_volume)
+VolumeKnob = SonosHW.RotaryEncoder(pinA=19, pinB=21, rotary_callback=WallboxRotaryControl.change_volume)
 
 # instance of the volume control button
 VolumeButton = SonosHW.PushButton(button_pin=25, callback=WallboxRotaryControl.pause_play_skip,
