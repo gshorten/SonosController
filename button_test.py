60ab80cd44399825155e4c3934e8877e7e72101a
gshorten <geoff.shorten@shortens.ca>
2019-05-09 16:35:09 -0600

    New interface board.

--- button_test.py
+++ button_test.py
@@ -3,7 +3,10 @@ test the button press
 """
 
 import SonosHW
+import i2cCharLCD
 
+# LCD on the wallbox
+ButtonLCD = i2cCharLCD.ExtendedAdafruitI2LCD()
 
 def button_test(button_type):
     if button_type == 'short':
@@ -12,7 +15,7 @@ def button_test(button_type):
     elif button_type == 'long':
         print ('Length: ', button_type)
         print('-------------------------------')
-
+    ButtonLCD.display_text('Button Duration', button_type)
 
 # little black button on front of volume control box; used to change sonos unit
 Button = SonosHW.PushButton(button_pin=18, short=1, callback=button_test, gpio_up_down='up',
