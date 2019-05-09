d65fce1fe57f3a5d69af4a659e04c9c384273996
gshorten <geoff.shorten@shortens.ca>
2019-05-09 11:47:39 -0600

    New interface board.

--- wallbox.py
+++ wallbox.py
@@ -38,7 +38,7 @@ VolumeKnob = SonosHW.RotaryEncoder(pinA=11, pinB=7, rotary_callback=WallboxRotar
 
 # instance of the volume control button
 VolumeButton = SonosHW.PushButton(button_pin=12, callback=WallboxRotaryControl.pause_play_skip,
-                                  gpio_up_down='up', short=.75, debounce=25)
+                                  gpio_up_down='down', short=.75, debounce=25)
 
 # little black button on front of volume control box; used to change sonos unit
 SelectUnitButton = SonosHW.PushButton(button_pin=18, short=.75, callback=Units.select_sonos_unit, gpio_up_down='up')
