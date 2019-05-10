7392592f85bc6c94126037a1372d4d114f4d7282
gshorten <geoff.shorten@shortens.ca>
2019-05-09 18:20:04 -0600

    New interface board.

--- button_test.py
+++ button_test.py
@@ -21,7 +21,7 @@ def button_test(button_type):
 
 # little black button on front of volume control box; used to change sonos unit
 Button = SonosHW.PushButton(button_pin=18, short=1, callback=button_test, gpio_up_down='up',
-                                      debounce=50)
+                                      debounce=30)
 
 
 while True:
