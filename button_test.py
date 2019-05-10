c13399a889425895ca59fd5de46e309516bbac4d
gshorten <geoff.shorten@shortens.ca>
2019-05-09 18:18:55 -0600

    New interface board.

--- button_test.py
+++ button_test.py
@@ -16,8 +16,8 @@ def button_test(button_type):
     elif button_type == 'long':
         print ('Length: ', button_type)
         print('-------------------------------')
-    ButtonLCD.display_text('Button Duration', button_type)
-    ButtonLCD.clear()
+    #ButtonLCD.display_text('Button Duration', button_type)
+    #ButtonLCD.clear()
 
 # little black button on front of volume control box; used to change sonos unit
 Button = SonosHW.PushButton(button_pin=18, short=1, callback=button_test, gpio_up_down='up',
