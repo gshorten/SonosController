858603ad2ab2eba365cf8a2521fc259d5f91266f
gshorten <geoff.shorten@shortens.ca>
2019-05-09 20:20:42 -0600

    New interface board.

--- SonosHWTest.py
+++ SonosHWTest.py
@@ -379,6 +379,7 @@ class PushButton:
         """
 
         self.button_down_time = time.time()
+        GPIO.remove_event_detect(self.pin)
         channel = GPIO.wait_for_edge(self.pin, GPIO.RISING, timeout = self.long_press )
         if channel is None:
             self.callback('long')
