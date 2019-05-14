604acb734faa0005dae1b8bf02ee5c143ba4eed6
gshorten <geoff.shorten@shortens.ca>
2019-05-13 20:15:04 -0600

    New button press algorithm, avoids segmentation faults. but timing is tricky...

--- SonosHWTest.py
+++ SonosHWTest.py
@@ -74,7 +74,6 @@ class PushButton:
         # handle both rising and falling - depends on if gpio pin on button is pulled high or low
         if self.gpio_up_down == 'up':
             GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-            # wait for the button to come up, using edge detect.
             channel = GPIO.wait_for_edge(self.pin, GPIO.RISING, timeout=self.long_press)
         else:
             GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
