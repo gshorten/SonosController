ea6275929be0786b9d699e098cd3bea3b1800c89
gshorten <geoff.shorten@shortens.ca>
2019-05-13 18:59:50 -0600

    debugging segmentation fault

--- button_test.py
+++ button_test.py
@@ -20,10 +20,11 @@ def button_test(duration):
     ButtonLCD.display_text('Button Duration', duration)
 
 
-
 # little black button on front of volume control box; used to change sonos unit
-Button = SonosHW.PushButton(button_pin=12, long_press=750, callback=button_test, gpio_up_down='down',
+BlackButton = SonosHW.PushButton(button_pin=18, long_press=.75, callback=button_test, gpio_up_down='up',
                                       debounce=50)
+VolumeButton = SonosHW.PushButton(button_pin=12, long_press=.75, callback=button_test,
+                                  gpio_up_down='down', debounce = 50)
 
 
 while True:
