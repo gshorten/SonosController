9c63aa9f7160d4016b09c33fa477d1f4612f53be
gshorten <geoff.shorten@shortens.ca>
2019-05-14 06:29:40 -0600

    New button press algorithm, avoids segmentation faults. but timing is tricky...

--- button_test.py
+++ button_test.py
@@ -23,14 +23,14 @@ def single_button_test():
     print("Button Pressed")
     ButtonLCD.clear()
     now = datetime.datetime.now()
-    now = now.strftime("%Y-%m-%d %M:%S")
+    now = now.strftime("%H:%M:%S")
     ButtonLCD.display_text("Button Pressed", now, sleep=5)
 
 
 
 # little black button on front of volume control box; used to change sonos unit
 BlackButton = SonosHW.SinglePressButton(pin=18, callback=single_button_test, gpio_up = 1,
-                                      debounce=150)
+                                      debounce=250)
 #VolumeButton = SonosHWTest.PushButton(button_pin=12, long_press=750, callback=button_test,
                                  # gpio_up_down='down', debounce = 20)
 
