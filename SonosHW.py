4ed09961d4c2e5fe16cfd8b6a60109bddd7b3920
gshorten <geoff.shorten@shortens.ca>
2019-05-14 07:47:57 -0600

    New button press algorithm, avoids segmentation faults. but timing is tricky...

--- SonosHW.py
+++ SonosHW.py
@@ -572,22 +572,23 @@ class DoublePushButton:
 
             self.callback(type)
 
-class SinglePressButton:
+class SinglePressButton():
     def __init__(self, pin, callback, debounce = 200, gpio_up = 1):
         self.callback = callback
         self.debounce = debounce
         self.gpio_up = gpio_up
         self.pin = pin
 
+
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
         # set up gpio pins for interrupt, accomodating pins pulled high or low.
         if self.gpio_up:
             GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-            GPIO.add_event_detect(self.pin, GPIO.FALLING, callback=self.button_press, bouncetime=self.debounce)
+            GPIO.add_event_detect(self.pin, GPIO.RISING, callback=self.button_press, bouncetime=self.debounce)
         else:
             GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-            GPIO.add_event_detect(self.pin, GPIO.RISING, callback=self.button_press, bouncetime=self.debounce)
+            GPIO.add_event_detect(self.pin, GPIO.FALLING, callback=self.button_press, bouncetime=self.debounce)
 
     def button_press(self,cb):
         self.callback()
