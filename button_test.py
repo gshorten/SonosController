f062a2605f3c41a9e720386555e05b1bd4ca2518
gshorten <geoff.shorten@shortens.ca>
2019-05-13 19:43:03 -0600

    New button press algorithm, avoids segmentation faults. but timing is tricky...

--- button_test.py
+++ button_test.py
@@ -20,7 +20,7 @@ def button_test(duration):
 
 # little black button on front of volume control box; used to change sonos unit
 BlackButton = SonosHW.PushButton(button_pin=18, long_press=1, callback=button_test, gpio_up_down='up',
-                                      debounce=100)
+                                      debounce=25)
 VolumeButton = SonosHW.PushButton(button_pin=12, long_press=1, callback=button_test,
                                   gpio_up_down='down', debounce = 20)
 
