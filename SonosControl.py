37d4eb54da0f7d40fdc9974e6bcc827118817de2
gshorten <geoff.shorten@shortens.ca>
2019-05-18 11:29:04 -0600

    Working on wallbox decode letter / number algorithm

--- SonosControl.py
+++ SonosControl.py
@@ -232,7 +232,7 @@ class CurrentTrack:
         if self.lcd.is_busy():
             # exit so we don't garble the display
             return
-        current = self.track_info()
+        current = self.currently_playing
         # use tryagain module to try calling self.track info if it fails.
         if current['title'] == self.old_title:
             return
