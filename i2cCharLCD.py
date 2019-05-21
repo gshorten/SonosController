77170e7a9a376e75fdbff9ef4b04a3416013c1b5
gshorten <geoff.shorten@shortens.ca>
2019-05-21 08:06:41 -0600

    Working on wallbox decode letter / number algorithm

--- i2cCharLCD.py
+++ i2cCharLCD.py
@@ -88,8 +88,8 @@ class ExtendedAdafruitI2LCD(adafruit_character_lcd.character_lcd_rgb_i2c.Charact
         try:
             # make sure strings are utf-8, ignore characters that are not
             # so that we do not scramble the display
-            line1 = line1.decode("utf-8", "ignore")
-            line2 = line2.decode("utf-8", "ignore")
+            line1 = line1.encode("utf-8", "ignore")
+            line2 = line2.encode("utf-8", "ignore")
             if line2 == 'nothing':
                 line2 = "                "
                 # replace "nothing" keyword with 16 spaces (so lcd does not display garbage)
