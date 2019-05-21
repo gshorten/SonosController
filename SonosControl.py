7a024f4958c5eccd92d8120090de1f42d7fea29b
gshorten <geoff.shorten@shortens.ca>
2019-05-21 11:32:20 -0600

    cleaning up documentation, removing uneeded code.

--- SonosControl.py
+++ SonosControl.py
@@ -380,7 +380,7 @@ class SonosUnits:
         print()
         print('List of Sonos Units and Names:')
         for i in self.units:
-           print( '{} {/8} {/20}'.format('Name: ', i.player_name, "Address: ", i.ip_address ))
+           print( '{0:5} {1:10} {2:20} {3:10}'.format('Name: ', i.player_name, "Address: ", i.ip_address ))
 
 
 class WallboxPlayer:
