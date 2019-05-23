810f7e66fca7f19fbc51ed66b1697b4875be1212
gshorten <geoff.shorten@shortens.ca>
2019-05-23 09:00:09 -0600

    cleaning up documentation, removing uneeded code.

---
+++ OLEDDisplay.py
@@ -0,0 +1,53 @@
+import time
+import subprocess
+
+from board import SCL, SDA
+import busio
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_ssd1306
+
+class OLEDDisplay:
+    """
+    Tiny little Adafruit OLED display
+    """
+    def __init__(self):
+        # Create the I2C interface.
+        i2c = busio.I2C(SCL, SDA)
+
+        # Create the SSD1306 OLED class.
+        # The first two parameters are the pixel width and pixel height.  Change these
+        # to the right size for your display!
+        self.disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+        # First define some constants to allow easy resizing of shapes.
+
+        width = self.disp.width
+        height = self.disp.height
+        self.image = Image.new('1', (width, height))
+        padding = -2
+        self.top = padding
+        self.bottom = height - padding
+        # Move left to right keeping track of the current x position for drawing shapes.
+        self.x = 0
+
+        self.draw = ImageDraw.Draw(self.image)
+
+    def clear_display(self):
+        # Clear display.
+        self.disp.fill(0)
+        self.disp.show()
+
+        # Create blank image for drawing.
+        # Make sure to create image with mode '1' for 1-bit color.
+
+
+        # Get drawing object to draw on image.
+        draw = ImageDraw.Draw(self.image)
+
+        # Draw a black filled box to clear the image.
+        draw.rectangle((0, 0, self.width, self.height), outline=0, fill=0)
+
+    def display_text(self, line1, line2):
+        font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 12)
+        self.clear_display()
+        self.draw.text((self.x, self.top + 0),line1, font=font, fill=255)
+        self.draw.text((self.x, self.top + 12), line2, font=font, fill=255)
---
+++ OLED_test.py
@@ -0,0 +1,15 @@
+"""
+OLED display test
+"""
+import OLEDDisplay
+import time
+
+Display = OLEDDisplay()
+
+while True:
+    Display.clear_display()
+    ti = time.gmtime()
+    line1 = " The time is:"
+    line2 = time.asctime(ti)
+    Display.display_text(line1, line2)
+    time.sleep(3)
