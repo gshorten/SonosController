dccbe84d44c9ab304eb7b004e5c60142449ddaea
gshorten <geoff.shorten@shortens.ca>
2019-05-23 13:01:32 -0600

    cleaning up documentation, removing uneeded code.

--- OLED_test.py
+++ OLED_test.py
@@ -8,8 +8,8 @@ Display = OLEDDisplay.OLED()
 
 while True:
     # Display.clear_display()
-    ti = time.gmtime()
+
     line1 = " The time is:"
-    line2 = time.asctime(ti)
+    line2 = time.ctime()
     Display.display_text(line1, line2)
     time.sleep(3)
