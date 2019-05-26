dfce2e142cd91f977048e8d3be51c605692bdc40
gshorten <geoff.shorten@shortens.ca>
2019-05-26 16:42:03 -0600

    initial commit

--- volctrl.py
+++ volctrl.py
@@ -61,7 +61,7 @@ VolumeButton = SonosHW.PushButtonShortLong(button_pin=24, callback=VCBRotaryCont
                                   gpio_up_down='down', long_press=1, debounce=25)
 
 # little black button on front of volume control box; used to change sonos unit
-SelectUnitButton = SonosHW.SinglePressButton(button_pin=13, callback=Units.select_sonos_unit, gpio_up=1)
+SelectUnitButton = SonosHW.SinglePressButton(button_pin=13, callback=Units.select_unit_single_press, gpio_up=1)
 
 # Something to show on the screen when vol control box starts up
 LCDDisplay.display_text("Volume Control", Units.active_unit.player_name, timeout=5, sleep=1)
