47823c6e48a8fdc3878523928abe1a87269e366d
gshorten <geoff.shorten@shortens.ca>
2019-05-29 07:03:35 -0600

    cleaning up code; trying to eliminate garbled LCD display

--- i2cCharLCD.py
+++ i2cCharLCD.py
@@ -107,7 +107,7 @@ class ExtendedAdafruitI2LCD(adafruit_character_lcd.character_lcd_rgb_i2c.Charact
             # set.cursor_position(0,1)
             # self.message = line2
             self.column_align = False
-            text = line1 + '\\n' + line2
+            text = line1 + '\n' + line2
             self.message = text
             time.sleep(sleep)
             self.display_start_time = time.time()
