1b250fa6c16689e4fce4925973ac62dfc61dd820
Geoff Shorten <geoff.shorten@shortens.ca>
2019-06-15 13:05:57 -0600

    fixing lcd timeou

--- i2cCharLCD.py
+++ i2cCharLCD.py
@@ -48,8 +48,8 @@ class ExtendedAdafruitI2LCD(adafruit_character_lcd.character_lcd_rgb_i2c.Charact
         super().__init__(i2c,lcd_columns,lcd_rows)
         self.display_start_time = time.time()
         # start timer def in seperate thread when instance is created.
-        self.timer_thread = threading.Thread(target=self.display_timeout)
-        self.timer_thread.start()
+        # self.timer_thread = threading.Thread(target=self.display_timeout)
+        # self.timer_thread.start()
 
     def is_busy(self, write_time = 2):
         """
@@ -129,7 +129,6 @@ class ExtendedAdafruitI2LCD(adafruit_character_lcd.character_lcd_rgb_i2c.Charact
 
         :param timeout:     turn off backlight after specified seconds
         :type timeout:      int
-
         """
         
         # do the time out loop here
