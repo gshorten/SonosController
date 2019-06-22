1f75dd4460dfb2c4e807f8777ed85c8f5f115342
gshorten <geoff.shorten@shortens.ca>
2019-06-22 08:49:29 -0600

    fixing display garbling

--- i2cCharLCD.py
+++ i2cCharLCD.py
@@ -105,7 +105,7 @@ class ExtendedAdafruitI2LCD(adafruit_character_lcd.character_lcd_rgb_i2c.Charact
             self.color = [100, 100, 100]
             # self.backlight = True
             self.clear()
-            self.set_cursor(0,0)
+            # self.set_cursor(0,0)
             # self.message = line1
             # set.cursor_position(0,1)
             # self.message = line2
