a3247ff5a7c01fe4fa22cf298b8df9b9b9789a81
gshorten <geoff.shorten@shortens.ca>
2019-06-22 09:09:20 -0600

    fixing display garbling

--- SonosUtils.py
+++ SonosUtils.py
@@ -21,14 +21,14 @@ def center_text(text):
     if text_length > 16:
         # truncate text if it is too long
         # also convert to a string for good measure, in case we pass an object!
-        text = str(text[0:15])
+        text = str(text[0:14])
     # calculate how much padding is required to fill display
-    padding = math.ceil((16 - text_length) / 2)
+    padding = math.ceil((15 - text_length) / 2)
     padding_text = " " * (padding)
     # pad the display text to center it.
     display_text = padding_text + text + padding_text
     # make sure it is still 16 characters long; take the first 16 characters
-    display_text = display_text[0:15]
+    display_text = display_text[0:14]
     return display_text
 
 
