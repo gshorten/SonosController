e859750bf1d6dd738894b1e75ae401e453ac6a1d
gshorten <geoff.shorten@shortens.ca>
2019-07-04 14:13:29 -0600

    modified OLED display class to enable multiple line displays

--- OLEDDisplay.py
+++ OLEDDisplay.py
@@ -14,16 +14,17 @@ class OLED:
     """
     Tiny little Adafruit OLED display
     """
-    def __init__(self):
+    def __init__(self,pixels_wide=128, pixels_high=32, font_size=14, lines=2 ):
         # Create the I2C interface.
         i2c = busio.I2C(board.SCL, board.SDA)
 
         # Create the SSD1306 OLED class.
         # The first two parameters are the pixel width and pixel height.  Change these
         # to the right size for your display!
-        self.disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+        self.disp = adafruit_ssd1306.SSD1306_I2C(pixels_wide, pixels_high, i2c)
         # First define some constants to allow easy resizing of shapes.
-
+        self.font_size = font_size
+        self.lines = lines
         self.width = self.disp.width
         self.height = self.disp.height
         self.image = Image.new('1', (self.width, self.height))
@@ -42,29 +43,26 @@ class OLED:
         # Clear display.
         self.disp.fill(0)
         self.disp.show()
-
         # Create blank image for drawing.
         # Make sure to create image with mode '1' for 1-bit color.
         # Get drawing object to draw on image.
         draw = ImageDraw.Draw(self.image)
-
         # Draw a black filled box to clear the image.
         draw.rectangle((0, 0, self.width, self.height), outline=0, fill=0)
 
-    def display_text(self, line1, line2 = ""):
-        font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSansCondensed.ttf', 14)
+    def display_text(self, line1, line2 = "", line3 = ""):
+        font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSansCondensed.ttf', self.font_size)
         self.clear_display()
         self.draw.text((self.x, self.top + 0),line1, font=font, fill=255)
-        self.draw.text((self.x, self.top + 15), line2, font=font, fill=255)
+        self.draw.text((self.x, self.top + self.font_size + 1), line2, font=font, fill=255)
+        if self.lines == 3:
+            self.draw.text((self.x, self.top + (2 * self.font_size +2)), line3, font=font, fill=255)
         # Display image.
         self.disp.image(self.image)
         self.disp.show()
         self.display_start_time = time.time()
 
-    def is_busy(self):
-        return False
-
-    def display_timeout(self, timeout=360):
+    def display_timeout(self, timeout=600):
         """
         Times out the display (turns off the backlight).  Starts when class instance is created.
 
@@ -75,34 +73,14 @@ class OLED:
         :type timeout:      int
         """
 
-        # do the time out loop here
         while True:
             elapsed = time.time() - self.display_start_time
             if elapsed >= timeout:
-                self.color = [0, 0, 0]
-                # self.backlight = False
+                self.clear_display()
                 print('display has timed out, backlight is off')
             else:
                 print('LCD timer, on time is: ', round(elapsed), ' seconds')
-            #   self.color = [100, 100, 100]
             time.sleep(30)
         return
 
-    def check_display_timeout(self, timeout=300):
-        """
-        Times out the display (turns off the backlight).
 
-        :param timeout:     turn off backlight after specified seconds
-        :type timeout:      int
-
-        Each time we write to display set a timer. if nothing has reset the timer then turn off the backlight.
-        This has to run in a loop in the main program.
-        """
-        # calculate how long the display has been on
-        display_on_time = time.time() - self.display_start_time
-        if display_on_time > timeout:
-            self.clear_display()
-
-    def clean_up(self):
-        """ Clean up display on shutdown."""
-        self.clear_display()
