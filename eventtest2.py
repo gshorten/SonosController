9a2324597debf13aa8ea92ecd7816e51bfcd1466
gshorten <geoff.shorten@shortens.ca>
2019-07-04 09:08:27 -0600

    testing new event detection

--- eventtest2.py
+++ eventtest2.py
@@ -10,7 +10,7 @@ old_playstate = ""
 while True:
 
     # loop continuously to listen for events
-    playstate = active.device.get_current_transport_info()
+    playstate = active.get_current_transport_info()
     if playstate != old_playstate:
        print(playstate)
 
