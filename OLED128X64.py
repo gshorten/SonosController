b7b2c19890b658733c02e7105c4d63e2359e4ad5
gshorten <geoff.shorten@shortens.ca>
2019-10-08 15:21:01 -0600

    changing wallbox to OLED display

--- OLED128X64.py
+++ OLED128X64.py
@@ -80,7 +80,7 @@ class OLED:
         self.busy = True
         line1 = SonosUtils.center_text(line1,self.char_wide)
         line2 = SonosUtils.center_text(line2,self.char_wide)
-        line3 = SonosUtils.center_text(time.strftime("%I:%M"),self.char_wide)
+        line3 = SonosUtils.center_text(time.strftime("%b %-d  %-I:%M %p"),self.char_wide)
         self.clear_display()
         self.draw.text((self.x, self.top + 1),line1, font=self.font, fill=255)
         self.draw.text((self.x, self.top + self.font_size + 4), line2, font=self.font, fill=255)
