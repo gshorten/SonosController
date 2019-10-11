b0387c5539df56884743508c41b23d97b7f2b47c
gshorten <geoff.shorten@shortens.ca>
2019-10-11 16:49:07 -0600

    added cpu temperature display

--- OutsideTemp.py
+++ OutsideTemp.py
@@ -4,7 +4,7 @@ import json
 def get_outside_temp(city_key = "5913490", api_key="1b2c8e00bfa16ce7a48f76c3570fd3a2"):
 
     base_url = "http://api.openweathermap.org/data/2.5/weather?"
-    complete_url = base_url +"appid=" + api_key + "id=" + city_key
+    complete_url = base_url + "id=" + city_key  +"&appid=" + api_key
     response = requests.get(complete_url)
     x = response.json()
     print(x)
