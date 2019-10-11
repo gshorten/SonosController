ec8e99b4fab131c49deb21e98d6d1ed87f52461a
gshorten <geoff.shorten@shortens.ca>
2019-10-11 16:42:59 -0600

    added cpu temperature display

--- OutsideTemp.py
+++ OutsideTemp.py
@@ -7,6 +7,7 @@ def get_outside_temp(city_key = "5913490", api_key="1b2c8e00bfa16ce7a48f76c3570f
     complete_url = base_url +"appid=" + api_key + "id=" + city_key
     response = requests.get(complete_url)
     x = response.json()
+    print(x)
     y = x['main']
     current_temperature = y["temp"]
     print("Current Temperature is:", str(current_temperature))
