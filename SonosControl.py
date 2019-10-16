ed60525ae4f3ef30022a0d1fd09132f2886d157e
gshorten <geoff.shorten@shortens.ca>
2019-10-16 13:32:39 -0600

    changed OLED display font

--- SonosControl.py
+++ SonosControl.py
@@ -128,7 +128,7 @@ class SonosDisplayUpdater:
                                                                                  'PLAYBACK PAUSED'):
                     # turn LED off
                     self.led.change_led('off')
-                    print('LED timed out, turning LED off')
+                    # print('LED timed out, turning LED off')
             except Exception as e:
                 print('There was an error in check for sonos changes:', e)
 
