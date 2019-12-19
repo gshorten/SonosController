bbf126f366397024a1624685e367b88f7861fd32
gshorten <geoff.shorten@shortens.ca>
2019-12-19 09:48:43 -0700

    setting up rfid reader

--- rfid_test.py
+++ rfid_test.py
@@ -2,18 +2,19 @@
 Rfid reader test
 '''
 
-import serial
-serial = serial.Serial("/dev/ttyUSB0", baudrate=9600)
-import time
+import sys
 
-code = ''
-print('starting loop')
+import time
+import serial
+
+ser = serial.Serial(port='/dev/ttyUSB0', baudrate = 9600, parity=serial.PARITY_NONE,
+                    stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS,timeout=1)
+counter=0
+
 while True:
-        data = serial.read()
-        print("reading rfid: ",data)
-        if data == '\r':
-                print(code)
-                code = ''
-        else:
-                code = code + data
-        time.sleep(1)
+ data = ser.read()
+ print"Data:",data
+ time.sleep(1)
+
+ # x=ser.readline()
+ # print(x)
