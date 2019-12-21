69390a098eb793b7b017c2082b3e8a2e1e29ede4
gshorten <geoff.shorten@shortens.ca>
2019-12-21 11:41:22 -0700

    setting up rfid reader

--- rfid_test.py
+++ rfid_test.py
@@ -6,7 +6,7 @@ import time
 import serial
 
 #ser = serial.Serial(port='/dev/ttyUSB0', baudrate = 9600)
-ser = serial.Serial(port='/dev/ttyS0', baudrate = 9600)
+ser = serial.Serial(port='/dev/ttyUSB0', baudrate = 9600)
                     # parity=serial.PARITY_NONE,
                     # stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS,timeout=1)
 print(ser)
