aed318edf0e1c45a1780ad321a9b9f5015772075
gshorten <geoff.shorten@shortens.ca>
2019-12-20 22:31:27 -0700

    setting up rfid reader

--- SonosControl.py
+++ SonosControl.py
@@ -623,7 +623,7 @@ class WallboxPlayer:
         self.playlists = wallbox_page_set['playlists']
 
 
-    def select_wallbox_pageset(self,pin,held_time):
+    def select_wallbox_pageset(self,pin,held_time = 0):
         '''
         Uses the black pushbutton to manually select the pageset
         :param duration:
