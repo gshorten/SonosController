920adb42f3b9d9845ef7f666532d9ced8a14096e
gshorten <geoff.shorten@shortens.ca>
2019-12-27 08:02:38 -0700

    setting up rfid reader

--- rfid_test.py
+++ rfid_test.py
@@ -14,6 +14,7 @@ while True:
             for i in range (20):
                 taginfo = reader.readTag()
                 print("tag info:",taginfo)
+                time.sleep(.1)
 
         except Exception as e:
             print("error reading tag:",e)
