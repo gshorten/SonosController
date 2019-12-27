c977e40b82bdbf32412fa7df77582415977d49f8
gshorten <geoff.shorten@shortens.ca>
2019-12-27 08:00:14 -0700

    setting up rfid reader

--- rfid_test.py
+++ rfid_test.py
@@ -16,7 +16,7 @@ while True:
 
         except Exception as e:
             print("error reading tag:",e)
-        reader.serialPort.flushInput()
+        # reader.serialPort.flushInput()
 
 
     # try:
