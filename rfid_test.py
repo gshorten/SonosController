7c14b20d945ac4375743a91aeaf450c67907746c
gshorten <geoff.shorten@shortens.ca>
2019-12-27 08:07:21 -0700

    setting up rfid reader

--- rfid_test.py
+++ rfid_test.py
@@ -9,21 +9,22 @@ import RFIDTagReader
 reader = RFIDTagReader.TagReader("/dev/ttyUSB0")
 while True:
 
-    taginfo = reader.readTag()
-    print ("tag: ",taginfo)
-    reader.serialPort.flushInput()
+    # taginfo = reader.readTag()
+    # print ("tag: ",taginfo)
+    # reader.serialPort.flushInput()
 
-    # response = input("Enter to read tag")
-    # if response:
-    #     try:
-    #         for i in range (20):
-    #             taginfo = reader.readTag()
-    #             print("tag info:",taginfo)
-    #             time.sleep(.1)
-    #
-    #     except Exception as e:
-    #         print("error reading tag:",e)
-    #     reader.serialPort.flushInput()
+    response = input("Enter to read tag")
+    if response:
+        try:
+            print("reading tag")
+            for i in range (20):
+                taginfo = reader.readTag()
+                print("tag info:",taginfo)
+                time.sleep(.1)
+
+        except Exception as e:
+            print("error reading tag:",e)
+        reader.serialPort.flushInput()
 
 
     # try:
