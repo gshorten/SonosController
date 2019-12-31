f6727d737a151e042706318162db80866b7f3e83
gshorten <geoff.shorten@shortens.ca>
2019-12-31 08:00:30 -0700

    setting up rfid reader

--- test.py
+++ test.py
@@ -4,3 +4,12 @@ import pyudev
 context = pyudev.Context()
 usb_thing = pyudev.Devices.from_device_file(context,'/dev/ttyUSB0')
 print(usb_thing)
+
+monitor = pyudev.Monitor.from_netlink(context)
+monitor.filter_by(usb_thing)
+
+def usb_event(action, device):
+    print(action, device)
+
+observer = pyudev.MonitorObserver(monitor, usb_event)
+observer.start()
