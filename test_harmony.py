09b04c1705adc640523fccdd71575a5d737d5b86
gshorten <geoff.shorten@shortens.ca>
2020-01-09 15:02:50 -0700

    minor code cleanup

---
+++ test_harmony.py
@@ -0,0 +1,25 @@
+import harmony
+
+def get_hub():
+    hub = harmony.harmonysock(host = '192.168.1.55',hubid = '14255516')
+    return hub
+
+
+class LogiHub:
+
+    def __init__(self, ip = '192.168.1.55',rport = '14255516'):
+        self.ip = ip
+        self.rport = rport
+        self.hub = harmony.harmonysock(host = self.ip, hubid=self.rport)
+
+    def get_hub(self):
+        self.hub = harmony.harmonysock(host=self.ip, hubid=self.rport)
+
+def main():
+    Great_Room = LogiHub()
+    Great_Room.get_hub()
+    # print out configuration
+    config = Great_Room.hub.getconfig()
+    for i in config:
+        print(i, config[i])
+
