8387902d4bc60ccd5fcccc9612cd112a6ae6ff17
gshorten <geoff.shorten@shortens.ca>
2020-01-09 18:50:07 -0700

    minor code cleanup

--- test_harmony.py
+++ test_harmony.py
@@ -25,7 +25,7 @@ def main():
     # Great_Room.get_hub()
     # print out configuration
     config = Great_Room.hub.getconfig()
-    dumper.dump(config['activity'][4]["controlGroup"][1]['function'][1])
+    dumper.dump(config['activity'][4]["controlGroup"][1]['function'][2])
 
 
 
